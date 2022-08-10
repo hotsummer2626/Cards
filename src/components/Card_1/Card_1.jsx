17650b170932159c9259fd8a631364c524f8313a
SUNNY <hotsummer2626@gmail.com>
2022-08-10 18:04:24 +1000

    add icons

--- src/components/Card_1/Card_1.jsx
+++ src/components/Card_1/Card_1.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import "./Card_1.scss";
 
 const Card_1 = () => {
@@ -6,10 +9,19 @@ const Card_1 = () => {
     <div className="container">
       <div className="color-background"></div>
       <div className="img"></div>
+      <div className="icon__heart">
+        <FontAwesomeIcon icon={faHeart} />
+      </div>
       <div className="card-info">
         <div className="card-info__name">Batman</div>
         <div className="card-info__date">25-10-2020</div>
-        <div className="card-info__price">$9999</div>
+        <div className="card-info__price-group">
+          <div className="card-info__price-group__old-price">$20000</div>
+          <div className="card-info__price-group__new-price">$9999</div>
+          <div className="icon__cart">
+            <FontAwesomeIcon icon={faCartShopping} />
+          </div>
+        </div>
       </div>
     </div>
   );
