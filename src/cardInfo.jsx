0507bc89dedbd547fc28d026394a1711bd67d1c9
SUNNY <hotsummer2626@gmail.com>
2022-08-11 16:57:27 +1000

    import data from outside

---
+++ src/cardInfo.jsx
@@ -0,0 +1,9 @@
+import batmanImg from "./assets/batman.webp";
+
+export const cardInfo = {
+  title: "Batman",
+  img: batmanImg,
+  date: "25-01-2020",
+  oldPrice: "$20000",
+  newPrice: "$9999",
+};
--- src/components/CardOne/CardOne.jsx
+++ src/components/CardOne/CardOne.jsx
@@ -4,20 +4,21 @@ import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import "./CardOne.scss";
 
-const CardOne = () => {
+const CardOne = ({ title, img, date, oldPrice, newPrice }) => {
+  let backgroundImg = `url(${img})`;
   return (
     <div className="container">
       <div className="color-background"></div>
-      <div className="img"></div>
+      <div className="img" style={{ backgroundImage: backgroundImg }}></div>
       <div className="icon icon__heart">
         <FontAwesomeIcon icon={faHeart} />
       </div>
       <div className="card-info">
-        <div className="card-info__name">Batman</div>
-        <div className="card-info__date">25-10-2020</div>
+        <div className="card-info__name">{title}</div>
+        <div className="card-info__date">{date}</div>
         <div className="card-info__price-group">
-          <div className="card-info__price-group__old-price">$20000</div>
-          <div className="card-info__price-group__new-price">$9999</div>
+          <div className="card-info__price-group__old-price">{oldPrice}</div>
+          <div className="card-info__price-group__new-price">{newPrice}</div>
           <div className="icon icon__cart">
             <FontAwesomeIcon icon={faCartShopping} />
           </div>
